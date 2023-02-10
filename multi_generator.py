b3f90c36e77e411f745b23363f16b1caf166a8c9
lglanville <lachlanglanville@gmail.com>
2023-02-10 16:47:37 +1100

    New file:   multi_generator.py new file:   multimedia_funcs.py

---
+++ multi_generator.py
@@ -0,0 +1,30 @@
+import sys
+import csv
+from emu_xml_parser import record
+import multimedia_funcs
+
+def replace_jpegs(input_xml, out_dir):
+    for r in record.parse_xml(input_xml):
+        ident = r['EADUnitID']
+        row = dict(EADUnitID=r['EADUnitID'])
+        folder = find_asset_folder(ident)
+        for tif in multimedia_funcs.find_assetset(folder)
+            jpeg = create_jpeg(tif, out_dir)
+            row['MulMultiMediaRef_tab(+).Multimedia'] = jpeg
+            row['MulMultiMediaRef_tab(+).DetSource'] = ident
+            row['MulMultiMediaRef_tab(+).MulTitle'] = r['EADUnitTitle']
+            yield row
+
+
+def main(input_xml, out_dir):
+    csv_file = Path(out_dir, "jpeg_replacements.csv")
+    with csv_file.open('w', encoding='utf-8-sig', newline='') as f:
+        fieldnames = [
+            'EADUnitID', 'MulMultiMediaRef_tab.Multimedia', 'MulMultiMediaRef_tab(+).DetSource', 'MulMultiMediaRef_tab(+).MulTitle']
+        multi_writer = csv.DictWriter(f, fieldnames=fieldnames)
+        multi_writer.writeheader()
+        for row in replace_jpegs(input_xml, out_dir):
+            multi_writer.writerow(row)
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
---
+++ multimedia_funcs.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import os
+import re
+import shutil
+import subprocess
+
+def create_jpeg(fpath, outdir):
+    fpath = Path(fpath)
+    outfile = Path(outdir, fpath.stem+'.jpg')
+    if not outfile.exists():
+        print("jpegging", fpath.name, "->", outfile.name)
+        subprocess.run(
+            [
+                'magick', 'convert', str(fpath), '-resize', '2048x2048^>',
+                '-quality', '65', '-depth', '8', '-unsharp',
+                '1.5x1+0.7+0.02', str(outfile)])
+    return outfile
+
+def find_asset_folder(ident):
+    base = r"\\research-cifs.unimelb.edu.au\9730-UniversityArchive-Shared\Digitised_Holdings\Registered"
+    try:
+        pre, mid, suf = ident.split('.')
+        folder = Path(base, pre, mid, suf)
+        if folder.exists():
+            return folder
+        else:
+            folder = Path(base, pre, mid, suf[1:])
+            if folder.exists():
+                return folder
+    except ValueError as e:
+        print(e)
+
+
+def find_asset(folder, index):
+    if folder is None:
+        return None
+    tifs = []
+    for root, _, files in os.walk(folder):
+        for file in files:
+            fpath = Path(root, file)
+            if fpath.suffix.lower() in ('.tif', '.tiff'):
+                tifs.append(fpath)
+    try:
+        return tifs[index]
+    except IndexError as e:
+        print(e)
+
+def find_assets(folder):
+    if folder is None:
+        return None
+    for root, _, files in os.walk(folder):
+        for file in files:
+            fpath = Path(root, file)
+            if fpath.suffix.lower() in ('.tif', '.tiff'):
+                yield fpath
