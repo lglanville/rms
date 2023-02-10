240aa2fe8ad631e17e4e83374b55460b8d304646
lglanville <lachlanglanville@gmail.com>
2023-02-10 15:15:37 +1100

    Changes to be committed: new file:   emu_xml_parser.py new file:   item_convert.py new file:   multimedia_replacer.py

---
+++ emu_xml_parser.py
@@ -0,0 +1,126 @@
+from lxml import etree
+import sys
+from pprint import pprint
+from collections.abc import MutableMapping
+import csv
+
+class record(dict):
+    def __repr__(self):
+        return super(record, self).__repr__()
+
+    @classmethod
+    def parse_tuple(cls, tuple_elem):
+        "Parse out a record within an EMu xml report to JSON"
+        new_rec = cls()
+        for elem in tuple_elem:
+            if elem.tag == 'atom':
+                new_rec[elem.attrib['name']] = elem.text
+            elif elem.tag == 'tuple':
+                new_rec[elem.attrib['name']] = cls.parse_tuple(elem)
+            elif elem.tag == 'table':
+                new_rec[elem.attrib['name']] = []
+                for record in elem.iterfind('tuple'):
+                    new_rec[elem.attrib['name']].append(cls.parse_tuple(record))
+        return new_rec
+
+    @classmethod
+    def parse_xml(cls, xml_doc):
+        "iterator to generate JSON EMu records from xml"
+        tree = etree.parse(xml_doc)
+        for record in tree.iterfind('tuple'):
+            yield cls.parse_tuple(record)
+
+    def merge_field(self, **kwargs):
+        """merge a given field based on kwargs - table fields are appended as
+        new levels, text fields are added as new lines"""
+        for key, val in kwargs.items():
+            if key.endswith('_tab'):
+                if self.get(key) is not None:
+                    self[key].append(val)
+                else:
+                    self[key] = [val]
+            else:
+                self[key] += '\n' + val
+
+    def migrate_field(self, source, dest):
+        "Migrate a field to another field, blanking the source field"
+        val = self[source]
+        if dest.endswith('_tab'):
+            self[dest] = [val]
+        else:
+            self[dest] = val
+        self[source] = None
+
+    def to_xml(self):
+        "Export a record as EMu xml"
+        root_element = etree.Element('tuple')
+        for key, value in self.items():
+            if type(value) == str:
+                etree.SubElement(root_element, 'atom', name=key).text = value
+            if type(value) == record:
+                tup_element = value.to_xml()
+                tup_element.attrib['name'] = key
+                root_element.append(tup_element)
+            elif type(value) == list:
+                tab_element = etree.SubElement(root_element, 'table', name=key)
+                for val in value:
+                    if type(val) != record:
+                        tup_element = etree.SubElement(tab_element, 'tuple')
+                        field, _ = key.split('_')
+                        etree.SubElement(tup_element, 'atom', name=field).text = val
+                    else:
+                        tab_element.append(val.to_xml())
+        return root_element
+
+    def flatten(self, parent_key='', sep='.'):
+        "Flatten the record JSON structure to a format suitable for CSV output"
+        items = []
+        for k, v in self.items():
+            new_key = parent_key + sep + k if parent_key else k
+            if isinstance(v, MutableMapping):
+                items.extend(v.flatten(new_key, sep=sep).items())
+            elif isinstance(v, list):
+                for x in v:
+                    items.extend(x.flatten(new_key, sep=sep).items())
+            else:
+                items.append((new_key, v))
+        row = {}
+        for field, value in items:
+            if value is None:
+                value = ''
+            if not row.get(field):
+                row[field] = value
+            else:
+                row[field] += '|' + value
+        return row
+
+    def print_xml(self):
+        return etree.tostring(self.to_xml(), pretty_print=True).decode()
+
+    @staticmethod
+    def serialise_to_xml(table, records, out_file):
+        table_elem = etree.Element('table', name=table)
+        for rec in records:
+            table_elem.append(rec.to_xml())
+        tree = etree.ElementTree(table_elem)
+        tree.write(
+            out_file, pretty_print=True, standalone=True, xml_declaration=True,
+            encoding='UTF-8')
+
+    @classmethod
+    def serialise_to_csv(cls, in_file, out_file):
+        fieldnames = set()
+        rows = []
+        for r in cls.parse_xml(in_file):
+            row = r.flatten()
+            fieldnames.update(row.keys())
+            rows.append(row)
+        fieldnames = list(fieldnames)
+        fieldnames.sort()
+        with open(out_file, 'w', encoding='utf-8-sig', newline='') as f:
+            writer= csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(rows)
+
+if __name__ == '__main__':
+    record.serialise_to_csv(sys.argv[1], sys.argv[2])
---
+++ multimedia_replacer.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+import os
+import re
+import shutil
+import subprocess
+import sys
+import csv
+from emu_xml_parser import record
+from PIL import Image
+
+BASE_DIR = "\\research-cifs.unimelb.edu.au\9730-UniversityArchive-Shared\Registered"
+
+def create_jpeg(fpath, outdir):
+    fpath = Path(fpath)
+    outfile = Path(outdir, fpath.stem+'.jpg')
+    if not outfile.exists():
+        print("jpegging", fpath.name, "->", outfile.name)
+        subprocess.run(
+            [
+                'magick', 'convert', str(fpath), '-resize', '2048x2048^>',
+                '-quality', '65', '-depth', '8', '-unsharp',
+                '1.5x1+0.7+0.02', str(outfile)])
+    return outfile
+
+def reformat(csv_file, dest_dir, jpeg_dir):
+    with open(csv_file, encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for file in row["unregistered_tif"].split('|'):
+                new_image = move_image(file, dest_dir, row['EADUnitID'])
+                jpeg = create_jpeg(new_image, jpeg_dir)
+                yield [row['irn'], row['EADUnitID'], jpeg]
+
+def find_asset_folder(ident):
+    base = r"\\research-cifs.unimelb.edu.au\9730-UniversityArchive-Shared\Digitised_Holdings\Registered"
+    try:
+        pre, mid, suf = ident.split('.')
+        folder = Path(base, pre, mid, suf)
+        if folder.exists():
+            return folder
+        else:
+            folder = Path(base, pre, mid, suf[1:])
+            if folder.exists():
+                return folder
+    except ValueError as e:
+        print(e)
+
+
+def find_asset(folder, index):
+    if folder is None:
+        return None
+    tifs = []
+    for root, _, files in os.walk(folder):
+        for file in files:
+            fpath = Path(root, file)
+            if fpath.suffix.lower() in ('.tif', '.tiff'):
+                tifs.append(fpath)
+    try:
+        return tifs[index]
+    except IndexError as e:
+        print(e)
+
+
+def make_assets(indir, outdir):
+    tifs = []
+    for root, _, files in os.walk(indir):
+        for file in files:
+            fpath = Path(root, file)
+            if fpath.suffix.lower() in ('.tif', '.tiff', '.dng'):
+                jpeg = create_jpeg(fpath, outdir)
+                yield jpeg
+
+def replace_jpegs(input_xml, out_dir):
+    for r in record.parse_xml(input_xml):
+        ident = r['EADUnitID']
+        row = dict(EADUnitID=r['EADUnitID'], EADUnitTitle=r['EADUnitTitle'])
+        for page, multimedia in enumerate(r.get("MulMultiMediaRef_tab")):
+            row['page'] = page
+            row['MulMultiMediaRef_tab.irn'] = multimedia.get("irn")
+            size = (
+                int(multimedia.get("ChaImageWidth")),
+                int(multimedia.get("ChaImageHeight"))
+                    )
+            row['size'] = "{}x{}".format(*size)
+            if max(size) < 2000:
+                print(ident, "page", page, "is insufficient size:", size)
+                folder = find_asset_folder(ident)
+                row['TIF_folder'] = folder
+                tif = find_asset(folder, page)
+                row['TIF'] = tif
+                if tif is not None:
+                    with Image.open(tif) as im:
+                        row['TIF_size'] = "{}x{}".format(*size)
+                        if size != im.size:
+                            print(im.size)
+                            jpeg = create_jpeg(tif, out_dir)
+                            row['status'] = "JPEG replaced"
+                            row['Multimedia'] = jpeg
+                        else:
+                            row['status'] = "TIF is insufficient quality"
+                else:
+                    yield(dict(EADUnitID=ident, page=page, status="Poor quality, no Tif found"))
+            else:
+                row['status'] = 'OK'
+            yield row
+
+def main(input_xml, out_dir):
+    csv_file = Path(out_dir, "jpeg_replacements.csv")
+    csv_log = Path(out_dir, "multimedia_log.csv")
+    with csv_file.open('w', encoding='utf-8-sig', newline='') as f, csv_log.open('w', encoding='utf-8-sig', newline='') as g:
+        fieldnames = [
+            'EADUnitID', 'EADUnitTitle', 'status', 'page', 'MulMultiMediaRef_tab.irn',
+            'size', 'TIF_folder', 'TIF', 'TIF_size', 'status', 'Multimedia']
+        multi_writer = csv.DictWriter(f, fieldnames=['irn', 'Multimedia'])
+        log_writer = csv.DictWriter(g, fieldnames=fieldnames)
+        multi_writer.writeheader()
+        log_writer.writeheader()
+        for row in replace_jpegs(input_xml, out_dir):
+            log_writer.writerow(row)
+            if row.get('Multimedia') is not None:
+                jpeg_row = dict(irn=row.get('MulMultiMediaRef_tab.irn'), Multimedia=row.get('MulMultiMediaRef_tab.irn'))
+                multi_writer.writerow(jpeg_row)
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
