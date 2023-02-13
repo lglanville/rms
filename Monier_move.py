5db08885de07a9d98d04a9b1a2dc852c56de305a
lglanville <lachlanglanville@gmail.com>
2023-02-13 16:43:04 +1100

    new file:   Monier_move.py modified:   multimedia_funcs.py

---
+++ Monier_move.py
@@ -0,0 +1,24 @@
+import sys
+import csv
+import multimedia_funcs
+BASE_DIR = r"\\research-cifs.unimelb.edu.au\9730-UniversityArchive-Shared\Digitised_Holdings\Registered"
+
+def move_and_jpeg(csv_file, out_dir):
+    with open(csv_file, encoding='utf-8-sig') as f:
+        reader = csv.DictReader(f)
+        yield reader.fieldnames
+        for row in reader:
+            tif = multimedia_funcs.move_image(row['fpath'], BASE_DIR, row['EADUnitID'])
+            jpeg = create_jpeg(tif, out_dir)
+            row['MulMultiMediaRef_tab(+).Multimedia'] = jpeg
+            yield row
+
+def main(in_file, out_file, out_dir):
+    with open(out_file, 'w', encoding='utf-8-sig', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in move_and_jpeg(in_file):
+            writer.writerow(row)
+
+if __name__ == '__main__':
+    main(*sys[1:])
--- multimedia_funcs.py
+++ multimedia_funcs.py
@@ -4,6 +4,34 @@ import re
 import shutil
 import subprocess
 
+
+def id_transform(number):
+    idents = re.findall(r'(\d{4})[,_.-](\d{2,4})[,_.-](\d{1,5})', number)
+    if len(idents) == 1:
+        pre = idents[0][0]
+        mid = '0' * (4 - len(idents[0][1])) + idents[0][1]
+        suf = '0' * (5 - len(idents[0][2])) + idents[0][2]
+        identifier = pre + '.' + mid + '.' + suf
+        return identifier
+
+
+def move_image(fpath, base_dir, ident):
+    fpath = Path(fpath)
+    newpath = Path(base_dir, *ident.split('.'), fpath.suffix.upper().strip('.'))
+    if not newpath.exists():
+        newpath.mkdir(parents=True)
+    page = 1
+    new_fname = '-'.join(ident.split('.'))+'-'+'0'*(5-len(str(page)))+str(page)+fpath.suffix
+    new_dest = newpath / new_fname
+    while new_dest.exists():
+        page += 1
+        new_fname = '-'.join(ident.split('.'))+'-'+'0'*(5-len(str(page)))+str(page)+fpath.suffix
+        new_dest = newpath / new_fname
+    shutil.copy2(fpath, new_dest)
+    print(fpath.name, '->', new_fname)
+    return new_dest
+
+
 def create_jpeg(fpath, outdir):
     fpath = Path(fpath)
     outfile = Path(outdir, fpath.stem+'.jpg')
@@ -15,6 +43,7 @@ def create_jpeg(fpath, outdir):
                 '-quality', '65', '-depth', '8', '-unsharp',
                 '1.5x1+0.7+0.02', str(outfile)])
     return outfile
+
 
 def find_asset_folder(ident):
     base = r"\\research-cifs.unimelb.edu.au\9730-UniversityArchive-Shared\Digitised_Holdings\Registered"
@@ -29,6 +58,7 @@ def find_asset_folder(ident):
                 return folder
     except ValueError as e:
         print(e)
+
 
 def find_assets(folder):
     if folder is None:
